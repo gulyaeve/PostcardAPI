105140c770ce5d11eadf39c11e704888aa773a73
gulyaeve <gulyaev.e@icloud.com>
2022-12-16 17:13:55 +0300

    Added postcard list

--- utils/system.py
+++ utils/system.py
@@ -7,6 +7,12 @@ async def get_categories() -> list | None:
     return categories
 
 
+async def get_postcards_list(category: str) -> list | None:
+    postcards_list = os.listdir(f"postcards/{category}")
+    postcards_list.remove(".DS_Store")
+    return postcards_list
+
+
 async def get_postcard_preview(category: str, template: str) -> bytes:
     with open(f'postcards/{category}/{template}/preview.png', "rb") as image:
         f = image.read()

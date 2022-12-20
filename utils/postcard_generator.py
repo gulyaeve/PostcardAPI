f41d00f7324868e8220c90ababdc550f4987a50a
gulyaeve <baddman@list.ru>
2022-12-20 18:20:15 +0300

    Added new year

--- utils/postcard_generator.py
+++ utils/postcard_generator.py
@@ -16,9 +16,13 @@ async def make_postcard(text: str, category: str, template: str) -> bytes:
     with open(f'postcards/{category}/{template}/page_temp_{current_time}.html', 'w') as file:
         file.write(page)
 
-    browser = await launch(defaultViewport={'width': 1440, 'height': 100, 'scale': 1},
-                           headless=True, ignoreHTTPSErrors=True, executablePath='/usr/bin/chromium',
-                           args=['--no-sandbox'])
+    browser = await launch(
+        defaultViewport={'width': 2000, 'height': 100, 'scale': 1},
+        headless=True,
+        ignoreHTTPSErrors=True,
+        executablePath='/usr/bin/chromium',
+        args=['--no-sandbox']
+    )
     page = await browser.newPage()
     await page.goto(f'file:///src/postcards/{category}/{template}/page_temp_{current_time}.html')
     await page.screenshot({'path': f'temp/postcard_{category}_{template}_{current_time}.png', 'fullPage': 'true'})
